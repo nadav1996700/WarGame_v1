bfbaba7f8be47e4db004c473648219bfffe49d5f
nadav1996700 <nadav1996700@gmail.com>
2020-08-22 18:44:04 +0300

    update method setProgress()

--- app/src/main/java/com/example/wargame_v1/MainActivity.java
+++ app/src/main/java/com/example/wargame_v1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.wargame_v1;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,25 +19,23 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int SUPERMAN_TURN = 1;
-    private static final int IRONMAN_TURN = 2;
+    private static final int PLAYER1_TURN = 1;
+    private static final int PLAYER2_TURN = 2;
     private static final int LARGE_ATTACK_POINTS = 50;
     private static final int MEDIUM_ATTACK_POINTS = 30;
     private static final int SMALL_ATTACK_POINTS = 10;
 
-    private ProgressBar superman_PB;
-    private ProgressBar ironman_PB;
-    private ImageView superman_image;
-    private ImageView ironman_image;
-    private Button largeAttack_superman;
-    private Button mediumAttack_superman;
-    private Button smallAttack_superman;
-    private Button largeAttack_ironman;
-    private Button mediumAttack_ironman;
-    private Button smallAttack_ironman;
-    private ArrayList<Button> superman_Buttons;
-    private ArrayList<Button> ironman_Buttons;
-    private int turn = SUPERMAN_TURN;
+    private ProgressBar player1_PB;
+    private ProgressBar player2_PB;
+    private Button largeAttack_player1;
+    private Button mediumAttack_player1;
+    private Button smallAttack_player1;
+    private Button largeAttack_player2;
+    private Button mediumAttack_player2;
+    private Button smallAttack_player2;
+    private ArrayList<Button> player1_Buttons;
+    private ArrayList<Button> player2_Buttons;
+    private int turn = PLAYER1_TURN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,15 +46,15 @@ public class MainActivity extends AppCompatActivity {
         setValues();
 
         /* add buttons to lists */
-        initialize_superman_list();
-        initialize_ironman_list();
+        initialize_player1_list();
+        initialize_player2_list();
 
         /* activate buttons */
-        activateButtons(superman_Buttons);
-        activateButtons(ironman_Buttons);
+        activateButtons(player1_Buttons);
+        activateButtons(player2_Buttons);
 
         /* disable ironman buttons -> superman start */
-        for(Button btn: ironman_Buttons)
+        for(Button btn: player2_Buttons)
             btn.setEnabled(false);
     }
 
@@ -67,10 +66,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View view) {
                     /* update progress bar after attack */
                     setProgressBar(view);
-
-
-                    /* if game over -> switch Activity
-                       else -> switch turn */
+                    /* if game over -> switch Activity, else -> switch turn */
                     if(!gameOver())
                         switchTurn();
                 }
@@ -78,32 +74,29 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /* update life bar */
     private void setProgressBar(View view) {
-        if (turn == SUPERMAN_TURN) {
-            switch (view.getId()) {
-                case R.id.main_BTN_10pt_superman:
-                    decreasePB(ironman_PB, SMALL_ATTACK_POINTS);
-                    break;
-                case R.id.main_BTN_30pt_superman:
-                    decreasePB(ironman_PB, MEDIUM_ATTACK_POINTS);
-                    break;
-                case R.id.main_BTN_50pt_superman:
-                    decreasePB(ironman_PB, LARGE_ATTACK_POINTS);
-                    break;
-            }
-        } else if (turn == IRONMAN_TURN) {
-            switch (view.getId()) {
-                case R.id.main_BTN_10pt_ironman:
-                    decreasePB(superman_PB, SMALL_ATTACK_POINTS);
-                    break;
-                case R.id.main_BTN_30pt_ironman:
-                    decreasePB(superman_PB, MEDIUM_ATTACK_POINTS);
-                    break;
-                case R.id.main_BTN_50pt_ironman:
-                    decreasePB(superman_PB, LARGE_ATTACK_POINTS);
-                    break;
-            }
+        switch (view.getId()) {
+            case R.id.main_BTN_10pt_player1:
+                decreasePB(player2_PB, SMALL_ATTACK_POINTS);
+                break;
+            case R.id.main_BTN_10pt_player2:
+                decreasePB(player1_PB, SMALL_ATTACK_POINTS);
+                break;
+            case R.id.main_BTN_30pt_player1:
+                decreasePB(player2_PB, MEDIUM_ATTACK_POINTS);
+                break;
+            case R.id.main_BTN_30pt_player2:
+                decreasePB(player1_PB, MEDIUM_ATTACK_POINTS);
+                break;
+            case R.id.main_BTN_50pt_player1:
+                decreasePB(player2_PB, LARGE_ATTACK_POINTS);
+                break;
+            case R.id.main_BTN_50pt_player2:
+                decreasePB(player1_PB, LARGE_ATTACK_POINTS);
+                break;
         }
+
     }
 
     private void decreasePB(ProgressBar pb, int points) {
@@ -115,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
 
     /* if game over -> send victory name to Victory Activity and switch activity */
     private boolean gameOver() {
-        if(superman_PB.getProgress() == 0 || ironman_PB.getProgress() == 0) {
+        if(player1_PB.getProgress() == 0 || player2_PB.getProgress() == 0) {
             disableButtons();
             Toast.makeText(MainActivity.this, "game done",
                     Toast.LENGTH_LONG).show();
@@ -125,70 +118,67 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void disableButtons() {
-        for(Button btn: superman_Buttons)
+        for(Button btn: player1_Buttons)
             btn.setEnabled(false);
-        for(Button btn: ironman_Buttons)
+        for(Button btn: player2_Buttons)
             btn.setEnabled(false);
     }
 
-    private void initialize_superman_list() {
-        superman_Buttons = new ArrayList<>();
-        superman_Buttons.add(largeAttack_superman);
-        superman_Buttons.add(mediumAttack_superman);
-        superman_Buttons.add(smallAttack_superman);
+    /* add buttons of player_1 to list */
+    private void initialize_player1_list() {
+        player1_Buttons = new ArrayList<>();
+        player1_Buttons.add(smallAttack_player1);
+        player1_Buttons.add(mediumAttack_player1);
+        player1_Buttons.add(largeAttack_player1);
     }
 
-    private void initialize_ironman_list() {
-        ironman_Buttons = new ArrayList<>();
-        ironman_Buttons.add(largeAttack_ironman);
-        ironman_Buttons.add(mediumAttack_ironman);
-        ironman_Buttons.add(smallAttack_ironman);
+    /* add buttons of player_2 to list */
+    private void initialize_player2_list() {
+        player2_Buttons = new ArrayList<>();
+        player2_Buttons.add(smallAttack_player2);
+        player2_Buttons.add(mediumAttack_player2);
+        player2_Buttons.add(largeAttack_player2);
     }
 
     private void setValues() {
-        superman_PB = findViewById(R.id.main_PB_supermanPB);
-        ironman_PB = findViewById(R.id.main_PB_ironmanPB);
-        largeAttack_superman = findViewById(R.id.main_BTN_50pt_superman);
-        mediumAttack_superman = findViewById(R.id.main_BTN_30pt_superman);
-        smallAttack_superman = findViewById(R.id.main_BTN_10pt_superman);
-        largeAttack_ironman = findViewById(R.id.main_BTN_50pt_ironman);
-        mediumAttack_ironman = findViewById(R.id.main_BTN_30pt_ironman);
-        smallAttack_ironman = findViewById(R.id.main_BTN_10pt_ironman);
-        superman_image = findViewById(R.id.main_IV_superman);
-        ironman_image = findViewById(R.id.main_IV_ironman);
+        player1_PB = findViewById(R.id.main_PB_player1);
+        player2_PB = findViewById(R.id.main_PB_player2);
+        largeAttack_player1 = findViewById(R.id.main_BTN_50pt_player1);
+        mediumAttack_player1 = findViewById(R.id.main_BTN_30pt_player1);
+        smallAttack_player1 = findViewById(R.id.main_BTN_10pt_player1);
+        largeAttack_player2 = findViewById(R.id.main_BTN_50pt_player2);
+        mediumAttack_player2 = findViewById(R.id.main_BTN_30pt_player2);
+        smallAttack_player2 = findViewById(R.id.main_BTN_10pt_player2);
+        ImageView player1_image = findViewById(R.id.main_IV_player1);
+        ImageView player2_image = findViewById(R.id.main_IV_player2);
 
         /* set images using glide library*/
-        setImages();
+        setImage(player1_image, ContextCompat.getDrawable(this, R.drawable.superman));
+        setImage(player2_image, ContextCompat.getDrawable(this, R.drawable.iron));
 
         /* initialize value of progress bars to 100 */
-        superman_PB.setProgress(100);
-        ironman_PB.setProgress(100);
+        player1_PB.setProgress(100);
+        player2_PB.setProgress(100);
     }
 
-    private void setImages() {
+    /* set images using glide library*/
+    private void setImage(ImageView iv, Drawable photo) {
         Glide.with(MainActivity.this)
-                .load("")
-                .placeholder(R.drawable.superman)
+                .load(photo)
                 .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .into(superman_image);
-
-        Glide.with(MainActivity.this)
-                .load("")
-                .placeholder(R.drawable.iron)
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .into(ironman_image);
+                .into(iv);
     }
 
     private void switchTurn() {
         ArrayList<Button> switchTo;
         ArrayList<Button> current;
-        if (turn == SUPERMAN_TURN) {
-            current = superman_Buttons;
-            switchTo = ironman_Buttons;
+        if (turn == PLAYER1_TURN) {
+            current = player1_Buttons;
+            switchTo = player2_Buttons;
         }
         else {
-            current = ironman_Buttons;
-            switchTo = superman_Buttons;
+            current = player2_Buttons;
+            switchTo = player1_Buttons;
         }
         makeSwitch(current, switchTo);
     }
@@ -202,9 +192,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void changeTurn() {
-        if (turn == SUPERMAN_TURN)
-            turn = IRONMAN_TURN;
+        if (turn == PLAYER1_TURN)
+            turn = PLAYER2_TURN;
         else
-            turn = SUPERMAN_TURN;
+            turn = PLAYER1_TURN;
     }
 }
